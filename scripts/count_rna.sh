728d33195734601b2227560b27e8477252abed06
Liz Rebboah <erebboah@uci.edu>
2021-02-26 09:24:13 -0800

    updated rna script

--- scripts/count_rna.sh
+++ scripts/count_rna.sh
@@ -3,7 +3,7 @@
 #SBATCH -A SEYEDAM_LAB       ## account to charge 
 #SBATCH -p standard          ## partition/queue name
 #SBATCH --nodes=1            ## (-N) number of nodes to use
-#SBATCH --array=1-79         ## number of tasks to launch
+#SBATCH --array=1-100         ## number of tasks to launch
 #SBATCH --cpus-per-task=2    ## number of cores the job needs
 #SBATCH --output=rna_count-%J.out ## output log file
 #SBATCH --error=rna_count-%J.err ## error log file
@@ -11,7 +11,7 @@
 module load subread/2.0.1
 gtf="/data/class/ecoevo283/erebboah/ref/dmel-all-r6.13.gtf"
 inpath="/data/class/ecoevo283/erebboah/RNAseq/"
-file=$inpath"prefixes_tissueE.txt" # prefix file
+file=$inpath"prefixes_random.txt" # prefix file
 prefix=`head -n $SLURM_ARRAY_TASK_ID  ${file} | tail -n 1`
 
 # -p paired end, -T ?? num threads, -t Specify feature type(s) in a GTF annotation, -g Specify attribute type in GTF annotation, -Q minimum mapping quality score, -F format of the provided annotation file, -a gene annot, -o output file name
