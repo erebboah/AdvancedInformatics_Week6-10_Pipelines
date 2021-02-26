c6a0d616dfb7b33c4ef68540f911a0d550bec07d
Liz Rebboah <erebboah@uci.edu>
2021-02-26 14:21:41 -0800

    update rna rscript

--- scripts/make_rna_matrix.R
+++ scripts/make_rna_matrix.R
@@ -3,15 +3,15 @@ library(data.table)
 setwd("/data/class/ecoevo283/erebboah/RNAseq")
 
 #################### Load metadata #################### 
-samples = read.table("/data/class/ecoevo283/erebboah/RNAseq/prefixes_tissueE.txt",header=FALSE)
+samples = read.table("/data/class/ecoevo283/erebboah/RNAseq/prefixes_random.txt",header=FALSE)
 samples = samples$V1
 
 #################### Read in .txt and make counts matrix across all samples in data folder ####################
 thisMat=as.data.frame(read.table(paste0("/data/class/ecoevo283/erebboah/RNAseq/counts/",samples[1],".counts.txt"),sep="",head=T, stringsAsFactors = F))
-head(thisMat)
+#head(thisMat)
 colnames(thisMat) = c("GeneID","Chr","Start","End","Strand","Length","SampleID")
 counts=thisMat[,c('GeneID','SampleID')]
-head(counts)
+#head(counts)
 
 for(i in 2:length(samples)){
   thisMat=as.data.frame(read.table(paste0("/data/class/ecoevo283/erebboah/RNAseq/counts/",samples[i],".counts.txt"),sep="",head=T, stringsAsFactors = F))
@@ -20,12 +20,12 @@ for(i in 2:length(samples)){
   counts=as.data.frame(cbind(counts,thisMat[,"SampleID"]))
 }
 
-dim(counts)
+#dim(counts)
 rownames(counts) = counts$GeneID
 counts$GeneID = NULL
 colnames(counts) = samples
-head(counts) 
+#head(counts) 
 
 # Save counts matrix with FlyBase IDs
-write.table(counts, file="/data/class/ecoevo283/erebboah/RNAseq/counts/counts_tissueE_flybaseIDs.tsv", quote=F)
+write.table(counts, file="/data/class/ecoevo283/erebboah/RNAseq/counts/counts_100samples_flybaseIDs.tsv", quote=F)
 
